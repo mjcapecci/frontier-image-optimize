a6901fe7b0980389744a09cdd132ce9221c0d090
Mike Capecci <mjcapecci@gmail.com>
2020-09-12 23:35:47 -0500

    adds marketing materials

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImages } from '@fortawesome/free-solid-svg-icons';
+import Logo from '../images/fwd-logo-black.png';
 
 const Header = () => {
   return (
-    <>
-      <h1 className='text-center mt-5'>
+    <div className='d-flex justify-content-center align-items-center flex-column mt-5'>
+      <img src={Logo} width='100px' alt='' />
+      <h1 className='text-center'>
         <FontAwesomeIcon icon={faImages}></FontAwesomeIcon> Image Optimize
       </h1>
-      <p className='text-center'>Frontier Web Development</p>
-    </>
+    </div>
   );
 };
 
